2d46705d9605625639ca8a35493e33083c479610
surfextreme111 <79855990+surfextreme111@users.noreply.github.com>
2024-06-04 13:42:58 +0200

    First Commit
    Api Documentation Login

---
+++ Modelos/Credenciales.cs
@@ -0,0 +1,9 @@
+﻿namespace DocumentationLogin3.Modelos {
+    public class Credenciales {
+        public int? id { get; set; }
+        public string name { get; set; }
+        public string pwd { get; set; }
+        public string role { get; set; }
+        public string token { get; set; }
+    }
+}
